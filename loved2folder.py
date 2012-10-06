e8ff473e3fe8a5cc1fd49c5d44c022e7a6702ab2
Bogdan Bucur <flaw_treisprezece@yahoo.com>
2012-10-06 19:21:49 +0300

    Improvements
    The list of songs is now written inside an xml file;
    Improved the parsing process: now the user is asked to enter the music
    folder path and  the file parsing can be skipped if there is already a
    songs.xml file

--- loved2folder.py
+++ loved2folder.py
@@ -4,8 +4,12 @@ import os
 
 import id3reader
 
+
+
 def listdir_fullpath(d):
     return [os.path.join(d, f) for f in os.listdir(d)]
+
+
 
 #parses all the files in a directory and returns an array with the filenames
 def parse_dirs(d):
@@ -16,7 +20,9 @@ def parse_dirs(d):
         for filename in filenames:
             allfiles.append(os.path.join(dirname, filename))
     return allfiles
-	
+    
+
+
 #makes sure the value is utf8 valid and an string
 def ensureutf8andstring(s):
     if isinstance(s, unicode):
@@ -27,62 +33,100 @@ def ensureutf8andstring(s):
         #s = s.encode('utf8')
     return str(s)
 
+
+
+#makes sure you receive a valid Yes or No answer
+def validYNAnswer(ans):
+    if ans == 'y':
+        return True
+    if ans == 'Y':
+        return True
+    if ans == 'Yes':
+        return True
+    if ans == 'yes':
+        return True
+    if ans == 'YES':
+        return True
+    if ans == 'N':
+        return False
+    if ans == 'n':
+        return False
+    if ans == 'No':
+        return False
+    if ans == 'no':
+        return False
+    if ans == 'NO':
+        return False
+
+    return None
+
+
+
+#gets the path to the music folder and parses the filelist, writing the result to songs.xml
+def getPathAndParseIt():
+    path = raw_input("Enter the path to your music folder: ")
+    if not os.path.exists(path):
+        while True:
+            path = raw_input("The folder specified does not exist. Enter the path to your music folder: ")
+            if not os.path.exists(path):
+                break
+
+    filelist = parse_dirs(path)
+
+    thelist  = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+    thelist += "<songs>\n"
+
+    print "There were " + str(len(filelist)) + " files found"
+    print "Please wait while the XML is built"
+
+    for filename in filelist:     
+        if filename[-4:] == ".mp3":
+            try:
+                id3r = id3reader.Reader(filename)
+            except UnicodeDecodeError:
+                artist = "None"
+                title = "None"
+            except IOError:
+                artist = "None"
+                title = "None"
+            else:
+                artist = ensureutf8andstring(id3r.getValue('performer'))
+                title = ensureutf8andstring(id3r.getValue('title'))
+    
+            if artist != "None" or title != "None":
+                #print (str(i) + ". " + artist + " - " + title)
+                thelist = thelist + "    <song path=\"" + filename + "\" artist=\"" + artist + "\" title=\"" + title + "\">" + artist + " - " + title + "</song>\n"
+
+    #sorted(thelist, key = str.lower)
+    thelist += "</songs>\n"
+
+    f = open('songs.xml','wb')
+    f.write(thelist)
+    f.close()
+    print "songs.xml was written"
+
 #clear the scren
 clear = lambda: os.system('cls')
 clear()
 
 print ("start")
 
-path = ("G:\\Muzica\\")
-#test_song = ("G:\\Muzica\\The Script - Breakeven.mp3")
+#we check to see if we already parsed the music folder
+if not os.path.exists("songs.xml"):
+    getPathAndParseIt()
+else:
+    #maybe we need to rebuild the songs list
+    validationMessage = "It seems your music folder was already parsed. Do you wish to update the songs list? Y / N: "
 
-filelist = parse_dirs(path)
+    while True:
+        reParse = raw_input(validationMessage).strip()
+        if not validYNAnswer(reParse) is None:
+            break
 
-#i = 0
-
-thelist = ""
-
-for filename in filelist:     
-    if filename[-4:] == ".mp3":
-        try:
-            id3r = id3reader.Reader(filename)
-        except UnicodeDecodeError:
-            artist = "None"
-            title = "None"
-        except IOError:
-            arist = "None"
-            title = "None"
-        else:
-            artist = ensureutf8andstring(id3r.getValue('performer'))
-            title = ensureutf8andstring(id3r.getValue('title'))
-    
-        if artist != "None" or title != "None":
-            #print (str(i) + ". " + artist + " - " + title)
-            thelist = thelist + artist + " - " + title + "\n"
-
-sorted(thelist, key = str.lower)
-
-f = open('out.txt','wb')
-f.write(thelist)
-f.close()
-
-"""
-filelist = listdir_fullpath(path)
-
-import id3reader
+    if validYNAnswer(reParse) == True:
+        getPathAndParseIt()
 
 
-"""
 
-
-"""
-for filename in filelist:
-    if filename[-4:] == ".mp3":
-        id3r = id3reader.Reader(filename)
-        artist = ensureutf8andstring(id3r.getValue('performer'))
-        title = ensureutf8andstring(id3r.getValue('title'))
-        if artist != "None" or title != "None":
-            print (artist + " - " + title)
-"""
 
 
