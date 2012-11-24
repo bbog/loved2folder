439e139d5b2b5e85732f2869d78c009b806106b0
Bogdan Bucur <flaw_treisprezece@yahoo.com>
2012-11-24 20:16:38 +0200

    added custom song Dictionary class
    credits:
    http://parand.com/say/index.php/2007/07/13/simple-multi-dimensional-dictionaries-in-python/

--- parsers/parsingUtils.py
+++ parsers/parsingUtils.py
@@ -2,3 +2,13 @@
 
 def getValue(parentNode, childName):
 	return parentNode.getElementsByTagName(childName)[0].childNodes[0].nodeValue
+
+# credits: http://parand.com/say/index.php/2007/07/13/simple-multi-dimensional-dictionaries-in-python/
+class SongDict(dict):
+    def __init__(self, default=None):
+        self.default = default
+
+    def __getitem__(self, key):
+        if not self.has_key(key):
+            self[key] = self.default()
+        return dict.__getitem__(self, key)
