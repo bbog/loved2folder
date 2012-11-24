4ebd14b629064f021263eabd32d3a60b22e56d1f
Bogdan Bucur <flaw_treisprezece@yahoo.com>
2012-11-24 20:18:33 +0200

    file updated
    rewritten the method to generate a custom loved songs dictionary

--- parsers/lovedParserXSPF.py
+++ parsers/lovedParserXSPF.py
@@ -4,13 +4,19 @@ import parsingUtils
 
 from xml.dom import minidom
 
+# returns a custom song dictionary
+# dict[artist][title] = 1 is the standard entry
+# we can use it to check if a song is loved (if dict[song's artist][song's title] is 1, the song is loved)
 def parseLovedXSPF(xmlFile):
 	xmldoc = minidom.parse(xmlFile)
 	itemlist = xmldoc.getElementsByTagName('track') 
+	
+	lovedSongsDict = parsingUtils.SongDict(dict)
+
 
 	for track in itemlist :
 		title = parsingUtils.getValue(track, 'title')
 		artist = parsingUtils.getValue(track, 'creator')
-		#print title + ' - ' + artist
+		lovedSongsDict[artist][title] = 1
 
-#parseLovedXSPF('CedikFlaw_lovedtracks.xspf')
+	return lovedSongsDict
